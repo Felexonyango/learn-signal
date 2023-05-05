5d021c4a71d20383af35e0bb9bc5366fe4db8e88
Felexonyango <felexonyango19@gmail.com>
2023-05-05 16:02:45 +0300

    added creatsignal data

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,33 @@ import styles from './App.module.css';
 
 const App: Component = () => {
   const [first, setFirst] = createSignal("JSON");
-  const [last, setLast] = createSignal("Bourne");
-  
-  createEffect(() => console.log(`${first()} ${last()}`));
+  const [count, setCount] = createSignal(0)
+const[data,setData]=createSignal([
+  {
+    name:"Test",
+    email:"test@example.com",
+  },
+  {
+    name:"data",
+    email:"test@example.com",
+  }
+])
+  const currentCount = count();
+  const handleClick =()=>{
+    setData([...data(), {name:"test1",email:"email@example.com"}])
+  }
+
   return (
     <div class={styles.App}>
+   <p> my name is :{first()}</p>
+   <button onClick={handleClick}>Add Item</button>
+   <ul>
+        {data().map((item) => (
+          <li>{item.name}</li>
+        ))}
+      </ul>
+
+  
    
     </div>
   );
